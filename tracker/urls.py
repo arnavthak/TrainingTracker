c7e35ec621f19c84b1bd69081c940d75f76fac3e
Arnav Thakrar <arnav.thakrar@gmail.com>
2024-06-02 17:43:03 -0400

    Clickable calendar implemented. Links to page which can be customized based on day.

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'tracker'
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path('add_training/', views.add_training, name='add_training'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the tracker index.")
+    return render(request, 'tracker/calendar.html')
+
+def add_training(request):
+    date = request.GET.get('date')
+    if date:
+        return HttpResponse(f"{date}")
+    else:
+        return HttpResponse("Could not access date data")
