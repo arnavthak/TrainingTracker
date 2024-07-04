79415b5e6117e3a2cd11fbbf83e7424a1ea61c79
Arnav Thakrar <arnav.thakrar@gmail.com>
2024-07-04 15:02:11 -0400

    Successfully set up url scheme for the viewer.

--- training_tracker/urls.py
+++ training_tracker/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("tracker/", include("tracker.urls")),
+    path("viewer/", include("viewer.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
