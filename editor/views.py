c4bc2ddacf96c4bbda5e43cd0661e3a2681c3aad
Arnav Thakrar <arnav.thakrar@gmail.com>
2024-07-15 10:33:13 -0400

    Edit note functionality working well, although it is not connected via the front-end to the viewer yet.

--- editor/views.py
+++ editor/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from tracker.models import Workout, Image, Video, Note
+from tracker.forms import WorkoutForm, ImageForm, VideoForm, NoteForm
+from django.urls import reverse
+
 
 # Create your views here.
 def edit_workout(request):
     return HttpResponse("Edit workout view!")
 
 def edit_note(request):
-    return HttpResponse("Edit note view!")
+    id = request.GET.get('id')
+    user = request.user
+
+    note = get_object_or_404(Note, id=id)
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            redirect_url = reverse('viewer:view_note') + '?id={}'.format(id)
+            return redirect(redirect_url)
+    else:
+        form = NoteForm(instance=note)
+    
+    return render(request, 'editor/edit_note.html', {'form': form})
